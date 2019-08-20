04c390319b275a2af7c8e629da40950316988168
Vincent Wiart <vincent.wiart@gmail.com>
2019-08-20 11:22:34 +0200

    fix request id

--- requestid/request_id.go
+++ requestid/request_id.go
@@ -15,5 +15,14 @@ func Store(parent context.Context, requestID string) context.Context {
 
 // Get returns the request ID from the context.
 func Get(ctx context.Context) string {
-	return ctx.Value(contextRequestID).(string)
+	v := ctx.Value(contextRequestID)
+	if v == nil {
+		return ""
+	}
+
+	requestID, ok := ctx.Value(contextRequestID).(string)
+	if !ok {
+		return ""
+	}
+	return requestID
 }
