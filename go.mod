818c0862dfd90cde4f47d0bdf792b058859cd0d5
Vincent Wiart <vincent.wiart@gmail.com>
2020-02-24 12:24:47 +0100

    adding a jsonvalidation middleware

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/m4rw3r/uuid v1.0.0
 	github.com/rs/zerolog v1.17.2
 	github.com/uber/jaeger-client-go v2.20.1+incompatible // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0
 	go.opencensus.io v0.22.2
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	google.golang.org/api v0.14.0 // indirect
---
+++ jsonvalidation/json_validation.go
@@ -0,0 +1,73 @@
+package jsonvalidation
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/wrapp/instrumentation/logs"
+	"github.com/xeipuuv/gojsonschema"
+)
+
+func badRequest(w http.ResponseWriter, r *http.Request, message, err string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusBadRequest)
+	type response struct {
+		Message string `json:"message,omitempty"`
+		Error   string `json:"error,omitempty"`
+	}
+
+	json.NewEncoder(w).Encode(response{
+		Message: message,
+		Error:   err,
+	})
+}
+
+// Middleware checks whether the request payload validates a given schema.
+func Middleware(r io.Reader) func(next http.Handler) http.Handler {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		logs.New(context.Background()).
+			Panic().
+			Err(err).
+			Msg("unable to load schema")
+	}
+
+	schemaLoader := gojsonschema.NewBytesLoader(b)
+	schema, err := gojsonschema.NewSchema(schemaLoader)
+	if err != nil {
+		logs.New(context.Background()).
+			Panic().
+			Err(err).
+			Msg("unable to load schema")
+	}
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			buffer, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				badRequest(w, r, "no payload", err.Error())
+				return
+			}
+
+			payload := gojsonschema.NewBytesLoader(buffer)
+			result, err := schema.Validate(payload)
+			if err != nil {
+				badRequest(w, r, "invalid payload", err.Error())
+				return
+			}
+
+			if !result.Valid() {
+				badRequest(w, r, "invalid payload", "")
+				return
+			}
+
+			// we need to reinject the body because it has been consumed previously
+			r.Body = ioutil.NopCloser(bytes.NewBuffer(buffer))
+			next.ServeHTTP(w, r)
+		})
+	}
+}
