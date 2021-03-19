295afec144af4edcb26dcbdaf779cda34079a462
aronbirkir <aron@meniga.com>
2021-03-19 09:05:49 +0100

    Validation error check (#22)
    * Add client validation error checking
    
    * Change error result
    
    * Error formatting
    
    * Error extensions GQL friendly
    
    * Return clean validationErrors
    
    * Refactor validation error message
    
    * Add tests

---
+++ client/errors.go
@@ -0,0 +1,71 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+const (
+	fieldErrMsg = "Context: '%s' Error:Field validation for '%s' failed. Expected: %s but given: %s"
+)
+
+// FieldError describes error of a field
+type FieldError struct {
+	Context  string `json:"context"`
+	Field    string `json:"field"`
+	Expected string `json:"expected"`
+	Given    string `json:"given"`
+}
+
+func (fe FieldError) Error() string {
+	return fmt.Sprintf(fieldErrMsg, fe.Context, fe.Field, fe.Expected, fe.Given)
+}
+
+// ValidationErrors is an array of FieldError's
+// for use in custom error messages post validation.
+type ValidationErrors struct {
+	Message string       `json:"message"`
+	Valid   bool         `json:"valid"`
+	Errors  []FieldError `json:"errors"`
+}
+
+// Error creates an error message from array of validation errors
+func (ve ValidationErrors) Error() string {
+	if ve.Message != "" {
+		return ve.Message
+	}
+
+	buff := bytes.NewBufferString("Validation Errors:\n")
+	var fe *FieldError
+	for i := 0; i < len(ve.Errors); i++ {
+		fe = &ve.Errors[i]
+		buff.WriteString(fe.Error())
+		buff.WriteString("\n")
+	}
+
+	return strings.TrimSpace(buff.String())
+}
+
+// Extensions provide a map of the error properties
+func (ve ValidationErrors) Extensions() map[string]interface{} {
+	m := map[string]interface{}{
+		"message": ve.Message,
+		"valid":   ve.Valid,
+		"fields":  ve.Errors,
+	}
+	return m
+}
+
+// ParseValidationErrors parses a byte array for validation errors
+func ParseValidationErrors(body []byte, message string) (ValidationErrors, error) {
+	var validationErrors ValidationErrors
+	if err := json.NewDecoder(bytes.NewBuffer(body)).Decode(&validationErrors); err != nil {
+		return ValidationErrors{}, fmt.Errorf("Failed to parse validation errors: %w", err)
+	}
+	if message != "" {
+		validationErrors.Message = message
+	}
+	return validationErrors, nil
+}
--- client/failure_option.go
+++ client/failure_option.go
@@ -1,6 +1,11 @@
 package client
 
-import "net/http"
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 // FailManager is a failure handler.
 type FailManager interface {
@@ -50,4 +55,39 @@ func StatusBetween(err error, min, max int) FailManager {
 // StatusIsNot creates a FailManager that fails if the status is not in the list.
 func StatusIsNot(err error, statusList ...int) FailManager {
 	return statusChecker{raiseErr: err, statusList: statusList, mustContain: true}
+}
+
+// HasValidationErrors creates a FailManager that fails if the status is StatusBadRequest
+// and the body includes validation errors.
+// The error returned will be of type ValidationErrors wrapped under the given error.
+func HasValidationErrors(err error) FailManager {
+	return validationErrorsChecker{raiseErr: err}
+}
+
+type validationErrorsChecker struct {
+	raiseErr error
+}
+
+func (checker validationErrorsChecker) Check(resp *http.Response) error {
+	if resp.StatusCode != http.StatusBadRequest {
+		return nil
+	}
+	// Retrieve the body
+	body, err := ioutil.ReadAll(resp.Body)
+	defer func(bdy []byte) {
+		// reinject the body because it has been consumed previously, in case someone wants to use it.
+		resp.Body = ioutil.NopCloser(bytes.NewBuffer(bdy))
+	}(body)
+	if err != nil {
+		return fmt.Errorf("%s: %w", checker.raiseErr.Error(), fmt.Errorf("Failed to read body to check for validation errors: %w", err))
+	}
+	validationErrors, err := ParseValidationErrors(body, checker.raiseErr.Error())
+	if err != nil {
+		return fmt.Errorf("%s: %w", checker.raiseErr.Error(), fmt.Errorf("Falied to parse validation errors from response body: %w", err))
+	}
+	// In the case the validation result is valid.
+	if validationErrors.Valid {
+		return nil
+	}
+	return validationErrors
 }
